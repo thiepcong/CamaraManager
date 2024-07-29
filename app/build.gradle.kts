c4e3df4fe16f60543e3db54bcd2c4743ecbd02d7
thiepcong12 <thiepcong12@gmail.com>
2024-07-29 17:00:31 +0700

    init project

---
+++ app/build.gradle.kts
@@ -0,0 +1,80 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.example.cameramanager"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.cameramanager"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // Concurrent library for asynchronous coroutines
+    implementation ("androidx.concurrent:concurrent-futures-ktx:1.1.0")
+
+    // Navigation library
+    val nav_version = "2.3.5"
+    implementation ("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation ("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    // CameraX core library
+    val camerax_version = "1.1.0-beta01"
+    implementation ("androidx.camera:camera-core:$camerax_version")
+
+    // CameraX Camera2 extensions
+    implementation ("androidx.camera:camera-camera2:$camerax_version")
+
+    // CameraX Lifecycle library
+    implementation ("androidx.camera:camera-lifecycle:$camerax_version")
+
+    // CameraX View class
+    implementation ("androidx.camera:camera-view:$camerax_version")
+
+    //WindowManager
+    implementation ("androidx.window:window:1.0.0-alpha09")
+
+    // Glide
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    kapt ("com.github.bumptech.glide:compiler:4.11.0")
+}
---
+++ cameraxmanager/src/main/java/com/example/cameraxmanager/CameraXManager.kt
@@ -0,0 +1,155 @@
+package com.example.cameraxmanager
+
+import android.content.Context
+import android.os.Build
+import android.util.Log
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.concurrent.futures.await
+import androidx.lifecycle.LifecycleOwner
+import java.nio.ByteBuffer
+import java.util.concurrent.ExecutorService
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+
+typealias LumaListener = (luma: Double) -> Unit
+class CameraXManager(
+    private val context: Context,
+    private val lifecycleOwner: LifecycleOwner,
+    private val windowManager: WindowManager,
+    private val cameraExecutor: ExecutorService
+) {
+
+    private var displayId: Int = -1
+    private var lensFacing: Int = CameraSelector.LENS_FACING_BACK
+    private var preview: Preview? = null
+    private var imageCapture: ImageCapture? = null
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var camera: Camera? = null
+    private var cameraProvider: ProcessCameraProvider? = null
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    suspend fun setUpCamera() {
+        cameraProvider = ProcessCameraProvider.getInstance(context).await()
+
+        lensFacing = when {
+            hasBackCamera() -> CameraSelector.LENS_FACING_BACK
+            hasFrontCamera() -> CameraSelector.LENS_FACING_FRONT
+            else -> throw IllegalStateException("Back and front camera are unavailable")
+        }
+
+        bindCameraUseCases()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    fun bindCameraUseCases() {
+        val metrics = windowManager.getCurrentWindowMetrics().bounds
+        Log.d(TAG, "Screen metrics: ${metrics.width()} x ${metrics.height()}")
+
+        val screenAspectRatio = aspectRatio(metrics.width(), metrics.height())
+        Log.d(TAG, "Preview aspect ratio: $screenAspectRatio")
+
+        val rotation = windowManager.defaultDisplay.rotation
+
+        val cameraProvider = cameraProvider
+            ?: throw IllegalStateException("Camera initialization failed.")
+
+        val cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+
+        preview = Preview.Builder()
+            .setTargetAspectRatio(screenAspectRatio)
+            .setTargetRotation(rotation)
+            .build()
+
+        imageCapture = ImageCapture.Builder()
+            .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+            .setTargetAspectRatio(screenAspectRatio)
+            .setTargetRotation(rotation)
+            .build()
+
+        imageAnalyzer = ImageAnalysis.Builder()
+            .setTargetAspectRatio(screenAspectRatio)
+            .setTargetRotation(rotation)
+            .build()
+            .also {
+                it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
+                    Log.d(TAG, "Average luminosity: $luma")
+                })
+            }
+
+        cameraProvider.unbindAll()
+
+        try {
+            camera = cameraProvider.bindToLifecycle(
+                lifecycleOwner, cameraSelector, preview, imageCapture, imageAnalyzer
+            )
+        } catch (exc: Exception) {
+            Log.e(TAG, "Use case binding failed", exc)
+        }
+    }
+
+    private fun aspectRatio(width: Int, height: Int): Int {
+        val previewRatio = max(width, height).toDouble() / min(width, height)
+        if (abs(previewRatio - RATIO_4_3_VALUE) <= abs(previewRatio - RATIO_16_9_VALUE)) {
+            return AspectRatio.RATIO_4_3
+        }
+        return AspectRatio.RATIO_16_9
+    }
+
+    private fun hasBackCamera(): Boolean {
+        return cameraProvider?.hasCamera(CameraSelector.DEFAULT_BACK_CAMERA) ?: false
+    }
+
+    private fun hasFrontCamera(): Boolean {
+        return cameraProvider?.hasCamera(CameraSelector.DEFAULT_FRONT_CAMERA) ?: false
+    }
+
+    companion object {
+        private const val TAG = "CameraXManager"
+        private const val RATIO_4_3_VALUE = 4.0 / 3.0
+        private const val RATIO_16_9_VALUE = 16.0 / 9.0
+    }
+
+    private class LuminosityAnalyzer(listener: LumaListener? = null) : ImageAnalysis.Analyzer {
+        private val frameTimestamps = ArrayDeque<Long>(5)
+        private val listeners = ArrayList<LumaListener>().apply { listener?.let { add(it) } }
+        private var lastAnalyzedTimestamp = 0L
+
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()
+            val data = ByteArray(remaining())
+            get(data)
+            return data
+        }
+
+        override fun analyze(image: ImageProxy) {
+            if (listeners.isEmpty()) {
+                image.close()
+                return
+            }
+
+            val currentTime = System.currentTimeMillis()
+            frameTimestamps.add(currentTime)
+
+            while (frameTimestamps.size >= 8) frameTimestamps.removeLast()
+
+            val buffer = image.planes[0].buffer
+            val data = buffer.toByteArray()
+            val pixels = data.map { it.toInt() and 0xFF }
+            val luma = pixels.average()
+
+            listeners.forEach { it(luma) }
+
+            image.close()
+        }
+    }
+}
